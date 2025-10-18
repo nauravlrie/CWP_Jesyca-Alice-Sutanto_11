11cee5ca99a8a8361bacb7487af0ddcfc064fb47
Naura Valerie <nauravlrie@gmail.com>
2025-10-18 23:22:00 +0700

    Update ForLoop.java
    yh

--- src/looping1/ForLoop.java
+++ src/looping1/ForLoop.java
@@ -3,9 +3,11 @@ package looping1;
 public class ForLoop {
 public int getNumbersAndSum(int maximalNumber) {
     int sum = 0;
+    // for loop digunakan untuk menjalankan bil dari 1 sampai maximalNumber
     for (int i = 1; i <= maximalNumber; i++) {
-        sum += i;
+        sum += i; // menambhakan i ke sum
     }
     return sum;
+    // return hasil sum
     }
 }
