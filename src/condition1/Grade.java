980acea273c17d5ebf52df9a36a8ae8830136b5d
Naura Valerie <nauravlrie@gmail.com>
2025-10-18 23:34:29 +0700

    Update Grade.java
    yh

--- src/condition1/Grade.java
+++ src/condition1/Grade.java
@@ -1,11 +1,13 @@
 package condition1;
 
 public class Grade {
+    // menampilkan grade berdasrkan nilai
 public String getGrade(int codeInputScore) {
+    // menggunakan if else
     if (codeInputScore >= 90) {
     return "Grade: A";
-    } else if (codeInputScore >= 75) {
-        return "Grade: B"; 
+    } else if (codeInputScore >= 75) { // jika nilai antara 75-89
+        return "Grade: B"; // akan menampilkan grade B
     } else if (codeInputScore >= 60) {
         return "Grade: C";
     }else {
