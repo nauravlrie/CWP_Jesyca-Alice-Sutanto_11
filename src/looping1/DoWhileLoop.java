fdc6f358a56c34bebe0ded34774fd2bb0044926e
Naura Valerie <nauravlrie@gmail.com>
2025-10-18 23:18:50 +0700

    Update DoWhileLoop.java
    yh

--- src/looping1/DoWhileLoop.java
+++ src/looping1/DoWhileLoop.java
@@ -2,19 +2,21 @@ package looping1;
 
 public class DoWhileLoop {
     public int getOddNumbersAndSum(int maximalNumber) {
-        int sum = 0; 
-        int i = 1; 
+        int sum = 0; // inisilaisasi variabel sum dengan 0
+        int i = 1; // inisialisasi variabel i dengan 1
         if (maximalNumber < 1) {
             return 0;
         }
+        // do while loop untuk menjumlahkan bilangan ganjil
             do {
-            if (i % 2 != 0) { 
-                sum += i; 
+            if (i % 2 != 0) { // mengecek apakah i adalah bil ganjil
+                sum += i; // menambahkan i ke sum jika i bil ganjil
             }
-            i++; 
-        } while (i <= maximalNumber); 
+            i++; // increment i
+        } while (i <= maximalNumber); // kondisi loop akan terus berlanjut jika i kurang dari atau sama dengan maximalNumber
             
           return sum;
+        // return hasil sum
     }
 }
 
