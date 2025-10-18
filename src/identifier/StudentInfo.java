26b48eec69967a1098a9ebe89185eacc70ce190b
nauravlrie <nauravlrie@gmail.com>
2025-10-18 10:12:01 +0700

    first commit

---
+++ src/identifier/StudentInfo.java
@@ -0,0 +1,9 @@
+package identifier;
+
+public class StudentInfo {
+    public String showStudentInfo(String codeInputName, int codeInputAge, double codeInputGpa) {
+    return "Name: " + codeInputName + "\n" +
+           "Age : " + codeInputAge + "\n" +
+           "GPA : " + codeInputGpa + "\n";
+    }
+}
---
+++ src/operator/Number.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class Number {
+public String getOperatorResults(int codeInputA, int codeInputB) {
+    return "Sum: " + (codeInputA + codeInputB) + "\n" + 
+           "Product: " + (codeInputA * codeInputB) + "\n" +
+           "Is Equal: " + (codeInputA == codeInputB) + "\n" +
+           "Logical AND: " + (codeInputA > 0 && codeInputB > 0) + "\n" +
+           "Logical OR: " + (codeInputA < 0 || codeInputB > 0);
+}
+    
+}
