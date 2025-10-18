9818eca4c5a0f5d60d8a3d90a052c8e54c201818
Naura Valerie <nauravlrie@gmail.com>
2025-10-18 23:24:35 +0700

    Update WhileLoop.java
    yh

--- src/looping1/WhileLoop.java
+++ src/looping1/WhileLoop.java
@@ -4,13 +4,15 @@ public class WhileLoop {
     public int getEvenNumbersAndSum(int maximalNumber) {
         int sum = 0;
         int i = 1;
-        while (i <= maximalNumber) {
-            if (i % 2 == 0) {  
-                sum += i; 
+        // while loop digunakan untuk menjalankan bil genap sampai maximalNumber
+        while (i <= maximalNumber) { // kondisi loop akan terus berlanjut selama i masih kurang dari atau sama dengan maximalNumber
+            if (i % 2 == 0) {  // mengecek apakah i adalah bil genap
+                sum += i; // menambahkan i ke sum jika i bil genap
             }
             
-            i++; 
+            i++; // increment i
         }
         return sum;
+        // return hasil sum
     }
 }
