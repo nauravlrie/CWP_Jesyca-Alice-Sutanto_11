44f789aa9c56a83a7293fb0d1e93b463e9303166
Naura Valerie <nauravlrie@gmail.com>
2025-10-18 23:04:27 +0700

    Update WhileLoopNested.java
    11

--- src/looping2/WhileLoopNested.java
+++ src/looping2/WhileLoopNested.java
@@ -2,20 +2,23 @@ package looping2;
 
 public class WhileLoopNested {
     public String getTriangle(int maximalNumber) {
+        // Menggunakan StringBuilder untuk efisiensi
         StringBuilder triangle = new StringBuilder();
         int row = 1;
-        while (row <= maximalNumber) { 
+        // loop untuk setiap baris
+        while (row <= maximalNumber) {  // kondisi terus berlanjut selama kurang dari atau sama dengan maximalNumber
             int col = 1;
-            while (col <= row) { 
-                triangle.append("*"); 
-                col++;
+            while (col <= row) { // kolom akan terus berlanjut selama seperti ketentuan
+                triangle.append("*"); // menambahkan bintang
+                col++; // increment
             }
             
             if (row < maximalNumber) {
                 triangle.append("\n");
             }
-            row++; 
+            row++; // increment baris untuk baris selanjutnya
         }
         return triangle.toString();
+        // return hasil segitigas bintangnya
     }
 }
