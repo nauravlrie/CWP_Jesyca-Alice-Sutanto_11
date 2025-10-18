c6fc906c293a1cbdb28142f06df0728532001030
Naura Valerie <nauravlrie@gmail.com>
2025-10-18 23:31:06 +0700

    Update NestedSwitch.java
    yh

--- src/condition2/NestedSwitch.java
+++ src/condition2/NestedSwitch.java
@@ -2,22 +2,23 @@ package condition2;
 
 public class NestedSwitch {
 public String getSelection(int category, int choice) {
-    switch (category) {
-        case 1:
+    // menentukan pilihan
+    switch (category) { // menggunakan switch case
+        case 1: // minuman
             switch (choice) {
-                case 2:
+                case 2: // coffee
                     return category + ", You selected: Coffee";
-                default:
+                default: // jika pil tidak diketahui
                     return category + ", invalid drink choice";
             }
-        case 2: 
+        case 2: // makanan
             switch (choice) {
-                case 1:
+                case 1: // pizza
                     return category + ", You selected: Pizza";
                 default:
                     return category + ", Invalid food choice";
             }
-                default:
+                default: // jika pil tidak dikenali
                     return category + ", Invalid category";
             }
         }
