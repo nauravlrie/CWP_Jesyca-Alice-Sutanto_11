bb774518570d269317d2439b3a90fb8a71c90945
nauravlrie <nauravlrie@gmail.com>
2025-10-18 22:32:56 +0700

    progress1

---
+++ src/basic/Calculator.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class Calculator {
+    //Untuk menjumlahkan dua bilangan bulat
+    public int add(int num1, int num2 ) {
+        // Mengembalikan hasil 
+    return num1 + num2;
+    }
+}
