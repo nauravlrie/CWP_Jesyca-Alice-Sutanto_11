67ed1d8993223cadc517da52191d9b8e4abdeab2
Naura Valerie <nauravlrie@gmail.com>
2025-10-18 23:39:14 +0700

    Update Array1D.java

--- src/array1/Array1D.java
+++ src/array1/Array1D.java
@@ -1,17 +1,20 @@
 package array1;
 
 public class Array1D {
+    // menampilkan apa saja arraynya dan jumlahnya
     public String getArrayInfo(int[] numbers) {
         StringBuilder result = new StringBuilder();
         int sum = 0;
-        for (int number : numbers) { 
-            result.append(number).append(" "); 
-            sum += number; 
+        // for loop untuk mengakses apa saja arraynya
+        for (int number : numbers) { // mengakses arraynya
+            result.append(number).append(" "); // menambahkan arraynya ke hasil output
+            sum += number; // menmbahkan ke jumlah 
         }
         if (result.length() > 0) {
             result.setLength(result.length() - 1);
         }
         result.append("\nSum: ").append(sum); 
         return result.toString(); 
+        // return hasil output
     }
 }
