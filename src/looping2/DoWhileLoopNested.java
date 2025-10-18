a43dfec00005e11c6cc207959618e8042bb4da2a
nauravlrie <nauravlrie@gmail.com>
2025-10-18 17:55:49 +0700

    progresss1

---
+++ src/looping2/DoWhileLoopNested.java
@@ -0,0 +1,5 @@
+package looping2;
+
+public class DoWhileLoopNested {
+    
+}
--- src/looping2/WhileLoopNested.java
+++ src/looping2/WhileLoopNested.java
@@ -2,24 +2,20 @@ package looping2;
 
 public class WhileLoopNested {
     public String getTriangle(int maximalNumber) {
-        // Menggunakan StringBuilder untuk efisiensi string agar tidak membuat banyak objek string baru
         StringBuilder triangle = new StringBuilder();
         int row = 1;
-        // loop untuk setiap baris
-        while (row <= maximalNumber) { // Kondisi baris berlanjut selama baris kurang dari atau sama dengan maxNumber
+        while (row <= maximalNumber) { 
             int col = 1;
-            while (col <= row) { // Kondisi kolom berlanjut selama kolom kurang dari atau sama dengan baris
-                triangle.append("*"); // Menambahkan bintang ke baris saat ini
-                col++; // Increment kolomnya untuk baris berikutnya
+            while (col <= row) { 
+                triangle.append("*"); 
+                col++;
             }
-            // Menambahkan baris baru jika itu bukan baris terakhir
+            
             if (row < maximalNumber) {
                 triangle.append("\n");
             }
-            row++; // Increment baris untuk baris berikutnya
+            row++; 
         }
-
-        // return hasil segitiga bintang nya
         return triangle.toString();
     }
 }
